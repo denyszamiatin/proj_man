d907ee992974326585bfc286f335ced127e271bc
Malfark <malfarfirst@gmail.com>
2017-02-09 14:11:32 +0200

    Предварительный вариант

--- project_manager.py
+++ project_manager.py
@@ -1,9 +1,7 @@
 """
-Project Manager v: 0.3.1
+Project Manager v: 0.4.1
 """
 import json
-
-
 
 class Database:
     def __init__(self, users_file='data_users', projects_file='data_projects'):
@@ -17,19 +15,29 @@ class Database:
     def get_users(self):
         return self.data["users"]
 
+
     def get_projects(self):
         return self.data["projects"]
 
-    def add_project_to_db(self, project):
-        json_project_obj = {}
-        for property in project.__dict__.keys():
-            if property != 'observers':
-                json_project_obj[property] = getattr(project, property)
 
-        if not json_project_obj in self.get_users():
+    def add_project_to_db(self, project):
+        json_project_obj = {atr : getattr(project, atr) for atr in project.__dict__.keys() if atr != 'observers'}
+
+        if not json_project_obj in self.get_projects():
             self.data["projects"].append(json_project_obj)
         else:
             raise KeyError('The project already exist')
+
+        self.update_db()
+
+    def add_user_to_db(self, user):
+        json_user_obj = {atr : getattr(user, atr) for atr in user.__dict__.keys()}
+
+        if not json_user_obj in self.get_users():
+            self.data["users"].append(json_user_obj)
+        else:
+            raise KeyError('This name is used')
+
         self.update_db()
 
 
@@ -47,16 +55,23 @@ class Database:
 
         self.update_db()
 
-    def add_user_to_db(self, user):
-        json_user_obj = {}
-        for property in user.__dict__.keys():
-            json_user_obj[property] = getattr(user, property)
 
-        if not json_user_obj in self.get_users():
-            self.data["users"].append(json_user_obj)
-        else:
-            raise KeyError('This name is used')
+
+    def update_project(self, name, property):
+        for project in self.get_projects():
+            if name == project['name'] and property[0] in project.keys():
+                project[property[0]] = property[1]
+
         self.update_db()
+
+
+    def update_user(self, name, property):
+        for user in self.get_users():
+            if name == user['login'] and property[0] in user.keys():
+                user[property[0]] = property[1]
+
+        self.update_db()
+
 
 
     def update_db(self):
@@ -74,51 +89,49 @@ class Environment:
     """
     def __init__(self, db):
         self.db = db
-        self.projects = {}
+
+    def get_users(self):
         self.users = {}
-
-
         for user in self.db.get_users():
             self.users[user['login']] = User(user['email'], user['login'], user['password'])
+
+        print (self.users)
+        return self.users
+
+
+    def get_projects(self):
+        self.projects = {}
         for project in self.db.get_projects():
-            self.projects[project['name']] = Project(project['name'], project['description'], project['owner'], ['project.members'])
+            self.projects[project['name']] = Project(project['name'], project['description'], project['owner'],
+                                                     ['project.members'])
+
+        print(self.projects)
+        return self.projects
+
 
     def add_project(self, project):
-        if project.name in self.projects:
-            raise KeyError(
-                'There is an existing project with such name. Use another one'
-            )
-        project.register_observer(self)
-        self.projects[project.name] = project
+        self.db.add_project_to_db(project)
 
-        self.db.add_project_to_db(project) #save projct to json file
 
     def delete_project(self, name):
-        try:
-            del self.projects[name]
-        except KeyError:
-            raise KeyError('There is no project with such name')
-        else:
-            self.db.del_project_from_db(name) #delete project from json file
+        self.db.del_project_from_db(name)
+
 
     def add_user(self, user):
-        if user.login in self.users:
-            raise KeyError('There is a user with such name. Use another one')
-        self.users[user.login] = user
-        self.db.add_user_to_db(user) #add user to json file
+        self.db.add_user_to_db(user)
+
 
     def delete_user(self, name):
-        try:
-            del self.users[name]
-        except KeyError:
-            raise KeyError('There is no user with such name')
-        else:
-            self.db.del_user_from_db(name) #delete user from json file
+        self.db.del_user_from_db(name)
 
-    def notify(self, old_name, project):
-        print(self.projects, old_name)
-        self.delete_project(old_name)
-        self.add_project(project)
+
+    def update_project(self, name, property):
+        self.db.update_project( name, property)
+
+
+    def update_user(self, name, property):
+        self.db.update_user(name, property)
+
 
 
 class Project:
@@ -129,28 +142,6 @@ class Project:
         self.owner = owner
         self.observers = []
 
-    def update_description(self, description):
-        self.description = description
-
-    def update_name(self, name):
-        old_name, self.name = self.name, name
-        for observer in self.observers:
-            print('Notify:', self.observers)
-            observer.notify(old_name, self)
-
-    def register_observer(self, observer):
-        if observer not in self.observers:
-            self.observers.append(observer)
-
-    def add_member(self, user):
-        self.members.append(user)
-
-    def delete_member(self, name):
-        try:
-            del self.members[name]
-        except KeyError:
-            raise KeyError('There is no user with such name')
-
 
 class User:
     def __init__(self, email, login, password):
@@ -158,9 +149,10 @@ class User:
         self.login = login
         self.password = password
 
-    def change_password(self, old_password, new_password):
-        if old_password != self.password:
-            raise ValueError('Password is wrong. Try again.')
-        self.password = new_password
 
-env = Environment(Database())
+
+if __name__ == "__main__":
+    env = Environment(Database())
+    env.get_projects()
+    env.get_users()
+
